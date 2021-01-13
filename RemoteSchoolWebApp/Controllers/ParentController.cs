ea2e558cf56bc41cc88d4ff8f0ebad712e81a80d
Maderaffie <szwonoliwia@gmail.com>
2021-01-13 23:51:12 +0100

    Photo uploading implemented

--- RemoteSchoolWebApp/Controllers/ParentController.cs
+++ RemoteSchoolWebApp/Controllers/ParentController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using RemoteSchoolWebApp.Data;
 using RemoteSchoolWebApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
+
 
 namespace RemoteSchoolWebApp.Controllers
 {
@@ -23,6 +29,24 @@ namespace RemoteSchoolWebApp.Controllers
             Parent parent = _schoolContext.Parents.SingleOrDefault(x => x.Email == userEmail);
             Student student = _schoolContext.Students.SingleOrDefault(x => x.ParentId == parent.Id);
             return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadPhoto(IFormFile image)
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            Parent parent = _schoolContext.Parents.SingleOrDefault(x => x.Email == userEmail);
+            Student student = _schoolContext.Students.SingleOrDefault(x => x.ParentId == parent.Id);
+            Image img;
+            using (var memoryStream = new MemoryStream())
+            {
+                await image.CopyToAsync(memoryStream);
+                img = Image.FromStream(memoryStream);
+                img.Save(memoryStream, img.RawFormat);
+                student.Image = memoryStream.ToArray();
+            }
+            await _schoolContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
