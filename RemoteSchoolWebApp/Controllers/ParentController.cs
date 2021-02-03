bd1c94bb6b4fcf860195bc158c3fcc8ef1a11e3f
Maderaffie <szwonoliwia@gmail.com>
2021-02-03 21:57:18 +0100

    Photo resizing added

--- RemoteSchoolWebApp/Controllers/ParentController.cs
+++ RemoteSchoolWebApp/Controllers/ParentController.cs
@@ -5,13 +5,14 @@ using RemoteSchoolWebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
-
+using SixLabors.ImageSharp.Formats;
 
 namespace RemoteSchoolWebApp.Controllers
 {
@@ -37,14 +38,22 @@ namespace RemoteSchoolWebApp.Controllers
             var userEmail = User.FindFirstValue(ClaimTypes.Email);
             Parent parent = _schoolContext.Parents.SingleOrDefault(x => x.Email == userEmail);
             Student student = _schoolContext.Students.SingleOrDefault(x => x.ParentId == parent.Id);
-            Image img;
-            using (var memoryStream = new MemoryStream())
+
+            using (var img = Image.Load(image.OpenReadStream(), out IImageFormat format))
             {
-                await image.CopyToAsync(memoryStream);
-                img = Image.FromStream(memoryStream);
-                img.Save(memoryStream, img.RawFormat);
-                student.Image = memoryStream.ToArray();
+                img.Mutate(img => img.AutoOrient());
+                int imgHeight = img.Height;
+                float imgWidth = img.Width;
+                float ratio = 360 / imgWidth;
+                int newHeight = (int)(imgHeight * ratio);
+                img.Mutate(x => x.Resize(360, newHeight));
+                using (var memoryStream = new MemoryStream())
+                {
+                    img.Save(memoryStream, format);
+                    student.Image = memoryStream.ToArray();
+                }
             }
+
             await _schoolContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
